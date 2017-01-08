ebedf7dd8067a21c77483a886afff88bca379a5b
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-01-08 21:36:55 +0000

    Completed practice

---
+++ practice.py
@@ -0,0 +1,224 @@
+"""
+Skills function practice.
+
+Please read the the instructions first (separate file). Your solutions should
+go below this docstring.
+
+PART ONE:
+
+    >>> hello_world()
+    Hello World
+
+    >>> say_hi("Balloonicorn")
+    Hi Balloonicorn
+
+    >>> print_product(3, 5)
+    15
+
+    >>> repeat_string("Balloonicorn", 3)
+    BalloonicornBalloonicornBalloonicorn
+
+    >>> print_sign(3)
+    Higher than 0
+
+    >>> print_sign(0)
+    Zero
+
+    >>> print_sign(-3)
+    Lower than 0
+
+    >>> is_divisible_by_three(12)
+    True
+
+    >>> is_divisible_by_three(10)
+    False
+
+    >>> num_spaces("Balloonicorn is awesome!")
+    2
+
+    >>> num_spaces("Balloonicorn is       awesome!")
+    8
+
+    >>> total_meal_price(30)
+    34.5
+
+    >>> total_meal_price(30, .3)
+    39.0
+
+    >>> sign_and_parity(3)
+    ['Odd', 'Positive']
+
+    >>> sign_and_parity(-2)
+    ['Even', 'Negative']
+
+PART TWO:
+
+    >>> full_title("Balloonicorn")
+    'Engineer Balloonicorn'
+
+    >>> full_title("Jane Hacks", "Hacker")
+    'Hacker Jane Hacks'
+
+    >>> write_letter("Jane Hacks", "Hacker", "Balloonicorn")
+    Dear Hacker Jane Hacks, I think you are amazing! Sincerely, Balloonicorn
+
+"""
+
+###############################################################################
+
+# PART ONE
+
+# 1. Write a function called 'hello_world' that does not take any arguments and
+#    prints "Hello World".
+
+def hello_world():
+    """Print "Hello World"""
+
+    print "Hello World"
+
+
+# 2. Write a function called 'say_hi' that takes a name as a string and
+#    prints "Hi" followed by the name.
+
+def say_hi(name):
+    """Take the parameter of a name and prints a greeting"""
+
+    print "Hi %s" % (name)
+
+
+# 3. Write a function called 'print_product' that takes two integers and
+#    multiplies them together. Print the result.
+
+def print_product(int1, int2):
+    """Print result of multiplying two integers."""
+
+    print int1 * int2
+
+# 4. Write a function called 'repeat_string' that takes a string and an integer
+#    and prints the string that many times
+
+def repeat_string(string, integer):
+    """Repeat string as many times as integer dictates"""
+
+    print string * integer
+
+
+# 5. Write a function called 'print_sign' that takes an integer and prints
+#    "Higher than 0" if higher than zero and "Lower than 0" if lower than zero.
+#    If the integer is zero, print "Zero".
+
+def print_sign(integer):
+    """Determine whether integer is greater or lower than 0"""
+
+    if integer > 0:
+        print "Higher than 0"
+    if integer < 0:
+        print "Lower than 0"
+    if integer == 0:
+        print "Zero"
+
+
+# 6. Write a function called 'is_divisible_by_three' that takes an integer and
+#    returns a boolean (True or False), depending on whether the number is
+#    evenly divisible by 3.
+
+def is_divisible_by_three(integer):
+    """Determine whether an integer is divisible by 3"""
+
+    if integer % 3 == 0:
+        return True
+    else:
+        return False
+
+
+# 7. Write a function called 'num_spaces' that takes a sentence as one string
+#    and returns the number of spaces.
+
+def num_spaces(string):
+    """Return number of spaces in a string"""
+
+    return string.count(" ")    
+
+
+# 8. Write a function called 'total_meal_price' that can be passed a meal price
+#    and a tip percentage. It should return the total amount paid
+#    (price + price * tip). **However:** passing in the tip percentage should
+#    be optional; if not given, it should default to 15%.
+
+def total_meal_price(price, tip=.15):
+    """Determine cost of a meal plus tip."""
+
+    total_tip = float(price * tip)
+    return float(price + total_tip)
+
+
+# 9. Write a function called 'sign_and_parity' that takes an integer as an
+#    argument and returns two pieces of information as strings --- "Positive"
+#    or "Negative" and "Even" or "Odd". The two strings should be returned in
+#    a list.
+#
+#    Then, write code that shows the calling of this function on a number and
+#    unpack what is returned into two variables --- sign and parity (whether
+#    it's even or odd). Print sign and parity.
+
+def sign_and_parity(integer):
+    
+    information = []
+
+    if integer % 2 == 0:
+        information.append("Even")
+        return "Odd"
+    elif integer % 2 != 0:
+        information.append("Odd")
+        return "Odd"
+    elif integer > 0:
+        information.append("Positive")
+        return "Positive"
+    elif integer < 0:
+        information.append("Negative")
+        return "Negative"
+    return information
+
+print sign_and_parity(12)
+
+
+###############################################################################
+
+# PART TWO
+
+# 1. Write a function that takes a name and a job title as parameters, making
+#    it so the job title defaults to "Engineer" if a job title is not passed
+#    in. Return the person's title and name in one string.
+
+def full_title(name, job="Engineer"):
+    """Return given job title and name"""
+
+    return "%s %s" % (job, name)
+
+# 2. Given a recipient name & job title and a sender name, print the following
+#    letter:
+#
+#       Dear JOB_TITLE RECIPIENT_NAME, I think you are amazing!
+#       Sincerely, SENDER_NAME.
+#
+#    Use the function from #1 to construct the full title for the letter's
+#    greeting.
+
+def write_letter(name, job, sender):
+    """Construct letter based on given parameters."""
+
+    print "Dear %s %s, I think you are amazing! Sincerely, %s" % (job, name, sender)
+
+
+###############################################################################
+
+# END OF PRACTICE: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
